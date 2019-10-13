a0a8d38f02c64cdd9ff485a3e8e99b0146ad07da
WuLab <38550511+ljw20180420@users.noreply.github.com>
2019-10-13 17:21:36 +0800

    Add files via upload

---
+++ 3D_sim_projects/4C/human_HEC-1-B/HEC-1-B_R1/set_sim.sh
@@ -0,0 +1,27 @@
+cd /home/ljw/new_fold/old_desktop/wuqiang/wuyonghu/3D_sim_projects/JINGWEI/4C/human_HEC-1-B/HEC-1-B_R1
+/home/ljw/new_fold/old_desktop/wuqiang/wuyonghu/3D_sim_projects/set_range_and_CTCF_ver3/build/set_range_and_ctcf_ver3 -start 140160700 -end 140920000 \
+-C4s_rep1 4C/4C_HEC-1-B_a12_R1_rep1.bedGraph,4C/4C_HEC-1-B_HS5-1_R1_rep1.bedGraph -C4s_rep2 4C/4C_HEC-1-B_a12_R1_rep2.bedGraph,4C/4C_HEC-1-B_HS5-1_R1_rep2.bedGraph \
+-CTCF_peaks chip/HEC-1-B_R1_CTCF.bam_peaks.txt -nipbls ~/new_fold/old_desktop/wuqiang/wuyonghu/3D_sim_projects/JINGWEI/4C/human_HEC-1-B/nipbl/Hec1bwt_nipbl_treat_afterfiting_chr5.bdg \
+-mut_inform mut_inform -chromosome chr5 -stiffnesses 2 -densities 0.2 -processivities 400 -separations 400 -D3_per_D1s 1250 -capture_ratios 2.000000 -record_1D 50000
+cd /home/ljw/new_fold/old_desktop/wuqiang/wuyonghu/3D_sim_projects/JINGWEI/4C/human_HEC-1-B/HEC-1-B_R1/run1
+/home/ljw/new_fold/old_desktop/wuqiang/wuyonghu/3D_sim_projects/loop_extrusion_3d_sim_ver5/build/loop_extrusion_3d_sim_ver5 \
+-pos_file /home/ljw/new_fold/old_desktop/wuqiang/wuyonghu/3D_sim_projects/JINGWEI/4C/human_HEC-1-B/HEC-1-B_R1/pos_file -THR_MAX 1 &
+cd /home/ljw/new_fold/old_desktop/wuqiang/wuyonghu/3D_sim_projects/JINGWEI/4C/human_HEC-1-B/HEC-1-B_R1/run2
+/home/ljw/new_fold/old_desktop/wuqiang/wuyonghu/3D_sim_projects/loop_extrusion_3d_sim_ver5/build/loop_extrusion_3d_sim_ver5 \
+-pos_file /home/ljw/new_fold/old_desktop/wuqiang/wuyonghu/3D_sim_projects/JINGWEI/4C/human_HEC-1-B/HEC-1-B_R1/pos_file -THR_MAX 1 &
+cd /home/ljw/new_fold/old_desktop/wuqiang/wuyonghu/3D_sim_projects/JINGWEI/4C/human_HEC-1-B/HEC-1-B_R1/run3
+/home/ljw/new_fold/old_desktop/wuqiang/wuyonghu/3D_sim_projects/loop_extrusion_3d_sim_ver5/build/loop_extrusion_3d_sim_ver5 \
+-pos_file /home/ljw/new_fold/old_desktop/wuqiang/wuyonghu/3D_sim_projects/JINGWEI/4C/human_HEC-1-B/HEC-1-B_R1/pos_file -THR_MAX 1 &
+cd /home/ljw/new_fold/old_desktop/wuqiang/wuyonghu/3D_sim_projects/JINGWEI/4C/human_HEC-1-B/HEC-1-B_R1/run4
+/home/ljw/new_fold/old_desktop/wuqiang/wuyonghu/3D_sim_projects/loop_extrusion_3d_sim_ver5/build/loop_extrusion_3d_sim_ver5 \
+-pos_file /home/ljw/new_fold/old_desktop/wuqiang/wuyonghu/3D_sim_projects/JINGWEI/4C/human_HEC-1-B/HEC-1-B_R1/pos_file -THR_MAX 1 &
+cd /home/ljw/new_fold/old_desktop/wuqiang/wuyonghu/3D_sim_projects/JINGWEI/4C/human_HEC-1-B/HEC-1-B_R1/run5
+/home/ljw/new_fold/old_desktop/wuqiang/wuyonghu/3D_sim_projects/loop_extrusion_3d_sim_ver5/build/loop_extrusion_3d_sim_ver5 \
+-pos_file /home/ljw/new_fold/old_desktop/wuqiang/wuyonghu/3D_sim_projects/JINGWEI/4C/human_HEC-1-B/HEC-1-B_R1/pos_file -THR_MAX 1 &
+cd /home/ljw/new_fold/old_desktop/wuqiang/wuyonghu/3D_sim_projects/JINGWEI/4C/human_HEC-1-B/HEC-1-B_R1/run6
+/home/ljw/new_fold/old_desktop/wuqiang/wuyonghu/3D_sim_projects/loop_extrusion_3d_sim_ver5/build/loop_extrusion_3d_sim_ver5 \
+-pos_file /home/ljw/new_fold/old_desktop/wuqiang/wuyonghu/3D_sim_projects/JINGWEI/4C/human_HEC-1-B/HEC-1-B_R1/pos_file -THR_MAX 1 &
+cd /home/ljw/new_fold/old_desktop/wuqiang/wuyonghu/3D_sim_projects/JINGWEI/4C/human_HEC-1-B/HEC-1-B_R1/run7
+/home/ljw/new_fold/old_desktop/wuqiang/wuyonghu/3D_sim_projects/loop_extrusion_3d_sim_ver5/build/loop_extrusion_3d_sim_ver5 \
+-pos_file /home/ljw/new_fold/old_desktop/wuqiang/wuyonghu/3D_sim_projects/JINGWEI/4C/human_HEC-1-B/HEC-1-B_R1/pos_file -THR_MAX 1 &
+wait
